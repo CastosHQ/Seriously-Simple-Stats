c349da39a0150ebfe17a35264d1b190394c2eeaa
Sergey Zakharchenko <zaharchenko.dev@gmail.com>
2023-04-11 09:23:56 +0100

    Version 1.5.0-beta

--- seriously-simple-stats.php
+++ seriously-simple-stats.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Seriously Simple Stats
- * Version: 1.4.1
+ * Version: 1.5.0-beta
  * Plugin URI: https://wordpress.org/plugins/seriously-simple-stats
  * Description: Integrated analytics and stats tracking for Seriously Simple Podcasting.
  * Author: Castos
@@ -27,7 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 use SeriouslySimpleStats\Classes\Stats;
 
-define( 'SSP_STATS_VERSION', '1.4.1' );
+define( 'SSP_STATS_VERSION', '1.5.0-beta' );
 define( 'SSP_STATS_DIR_PATH', plugin_dir_path( __FILE__ ) );
 
 require_once SSP_STATS_DIR_PATH . 'vendor/autoload.php';
